03a8993adde20550a0720969242a918bc0a49f06
Unknown <mathies-h@hotmail.com>
2019-01-11 11:41:28 +0100

    Updated labels

--- testLabels.py
+++ testLabels.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 
-np.save("./output/testLabels", np.array([0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1]))
+np.save("./output/testLabels", np.array([0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1]))
 
