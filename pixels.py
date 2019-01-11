2ab81f78f4cf6c2c9f8a06aaeb06092eabdd21e7
Holst <notholst@gmail.com>
2019-01-11 10:55:22 +0100

    Fixed dataset structure

--- pixels.py
+++ pixels.py
@@ -1,44 +1,54 @@
 from PIL import Image
 import numpy as np
-import os, re
+import os
+import re
+
+'''
+[ images
+    [ image
+        [ row 
+            [ R, G, B ], [ R, G, B ]
+        ],
+        [ row 
+            [ R, G, B ], [ R, G, B ]
+        ]
+    ]
+]
+'''
 
 
 def getpixels(imageFile):
-    img = Image.open("./testSet/0.jpg").convert("RGB")
+    img = Image.open(imageFile).convert("RGB")
     data = img.getdata()
 
-    red = []
-    blue = []
-    green = []
+    output = []
 
     width, height = data.size
     for y in range(0, width):
+        row = []
         for x in range(0, height):
             p = img.getpixel((x, y))
-            red.append(p[0])
-            green.append(p[1])
-            blue.append(p[2])
+            row.append([p[0], p[1], p[2]])
+        output.append(row)
 
-    return red + green + blue
+    return output
+
+# imageDataArray = [None]
+# imageDataArray[0] = getpixels('./testImage.jpg')
+# output = np.array(imageDataArray)
+# print(output)
 
 
-for root, dirs, files in os.walk("./testSet"):
-    imageDataArray = [None] * len(files)
-    for filename in files:
-        pixels = getpixels(filename)
-        match = re.search("(\d*)", filename)
-        index = int(match.group(0))
-        imageDataArray[index] = pixels
-    output = np.array(imageDataArray)
-    np.save("./output/testSet", output)
+def generateDataFile(dirName):
+    for root, dirs, files in os.walk("./"+dirName):
+        imageDataArray = [None] * len(files)
+        for filename in files:
+            pixels = getpixels(os.path.join(root, filename))
+            match = re.search(r"(\d*)", filename)
+            index = int(match.group(0))
+            imageDataArray[index] = pixels
+        np.save("./output/" + dirName, np.array(imageDataArray))
 
-for root, dirs, files in os.walk("./trainingSet"):
-    imageDataArray = [None] * len(files)
-    for filename in files:
-        pixels = getpixels(filename)
-        match = re.search("(\d*)", filename)
-        index = int(match.group(0))
-        imageDataArray[index] = pixels
-    output = np.array(imageDataArray)
-    np.save("./output/trainingSet", output)
 
+generateDataFile('testSet')
+generateDataFile('trainingSet')
