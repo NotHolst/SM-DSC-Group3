985b216fef88d7504c7cb568614f0be8165a221f
Holst <notholst@gmail.com>
2019-01-16 14:19:15 +0100

    removed duplicates

--- image-augmenter/main.js
+++ image-augmenter/main.js
@@ -45,34 +45,42 @@ let randomNumbers = new Array(400)
   for(let j = 0; j < randomNumbers.length; j++){
     randomNumbers[j] = j
   }
-  randomNumbers = _.shuffle(randomNumbers)
+  randomNumbers = _.shuffle(_.shuffle(randomNumbers))
 
 for (let i = 0; i < inputFilesNoMasks.length; i++) {
   let filename = inputFilesNoMasks[i];
   let file = path.join(INPUT_DIR, filename);
 
   const fileNumber = /(\d*).jpg/.exec(filename);
-  const maskRegex = new RegExp(fileNumber[1] + "_mask.png", "i");
+  const maskRegex = new RegExp("^" + fileNumber[1] + "_mask.png", "i");
   
+
+
   let maskFile = path.join(INPUT_DIR, inputFiles.find(x => maskRegex.test(x)));
 
 
 
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 0);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 0);
+  
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 90);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 90);
+  
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 180);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 180);
+  
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 270);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 270);
 
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 0, true);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 0, true);
+  
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 90, true);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 90, true);
+  
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 180, true);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 180, true);
+  
   augmentImage(file, path.join(OUTPUT_IMAGES, randomNumbers[++outputIndex] + ".jpg"), 270, true);
   augmentImage(maskFile, path.join(OUTPUT_MASKS, randomNumbers[outputIndex] + ".png"), 270, true);
   
